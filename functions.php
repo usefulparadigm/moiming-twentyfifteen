1ab1b9b50ce7969ab3952055ae8d4088fb63df3b
Sukjoon Kim <sjoonk@gmail.com>
2015-10-29 16:00:26 +0900

    update for part4

--- functions.php
+++ functions.php
@@ -1,18 +1,14 @@
 <?php
 
-// add_action( 'wp_enqueue_scripts', 'twentyfifteen_parent_theme_enqueue_styles' );
-//
-// function twentyfifteen_parent_theme_enqueue_styles() {
-//     wp_enqueue_style( 'twentyfifteen-style', get_template_directory_uri() . '/style.css' );
-//     wp_enqueue_style( '-style',
-//         get_stylesheet_directory_uri() . '/style.css',
-//         array('twentyfifteen-style')
-//     );
-//
-// }
-
 // Hide admin bar if user is not admin
 add_filter( 'show_admin_bar' , 'my_function_admin_bar' );
 function my_function_admin_bar(){
 	return current_user_can( 'manage_options' );
 }
+
+// 참가신청 폼에 히든 필드로 Post ID 추가하기
+add_action( 'frm_entry_form', 'my_frm_entry_form' );
+function my_frm_entry_form( $form ) {
+  echo '<input type="hidden" name="post_id" value="'.get_the_ID().'">';
+}
+
---
+++ single-moim.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * The template for displaying single moim
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+        ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        	<?php
+        		// Post thumbnail.
+        		twentyfifteen_post_thumbnail();
+        	?>
+
+        	<header class="entry-header">
+        		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        	</header><!-- .entry-header -->
+
+        	<div class="entry-content">
+        		<?php the_content(); ?>
+        	</div><!-- .entry-content -->
+
+        	<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
+
+        </article><!-- #post-## -->
+
+        <div id="attendants" class="comments-area">
+    		<h2 class="comments-title">참가신청</h2>
+            <?php if ( is_user_logged_in() ): ?>
+                <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 11 ) ); ?>
+            <?php else: ?>
+                <p class="info">
+                    참가신청를 하려면 
+                    <a href="<?php echo wp_login_url( get_permalink().'#attendants' ); ?>" title="Login">로그인</a>
+                    하세요!
+                </p>    
+            <?php endif; ?>
+            
+            <?php echo FrmProDisplaysController::get_shortcode( array( 'id' => 97 ) ); ?>
+        </div>    
+        
+        <?php
+			/*
+			 * Include the post format-specific template for the content. If you want to
+			 * use this in a child theme, then include a file called called content-___.php
+			 * (where ___ is the post format) and that will be used instead.
+			 */
+            // get_template_part( 'content', 'moim' );
+            
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+			// Previous/next post navigation.
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
+
+		// End the loop.
+		endwhile;
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
